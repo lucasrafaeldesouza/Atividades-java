9ca01a48d65c28904487f6dd62620b45879ab0c6
lucasrafaeldesouza <56982485+lucasrafaeldesouza@users.noreply.github.com>
2021-08-30 12:06:37 -0300

    Update and rename atividade02_area.txt to atividade02_area.java

--- atividade02_area.java
+++ atividade02_area.java
@@ -1,11 +1,9 @@
-public class area {
-      public static void main(String args[]) {
-        
+public class area{
+      public static void main(String args[]){
         
         float lado01 = 200;
         float lado02 = 100;
        
-        
         float area = (lado01 * lado02);
         
         System.out.println(area);
