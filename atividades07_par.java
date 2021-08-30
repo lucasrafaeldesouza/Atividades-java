5979818eabf0f62ab841864705b9dafa0d49328d
lucasrafaeldesouza <56982485+lucasrafaeldesouza@users.noreply.github.com>
2021-08-30 12:09:54 -0300

    Update and rename atividades07_par.txt to atividades07_par.java

---
+++ atividades07_par.java
@@ -0,0 +1,11 @@
+public class par{
+      public static void main(String args[]){
+        
+       int par  = 6;// par
+       int impar  = 3;// impar
+       
+       String message =  (par %2 == 1) ? "É IMPAR" : "É PAR";
+      
+       System.out.println(message); 
+    }
+}
