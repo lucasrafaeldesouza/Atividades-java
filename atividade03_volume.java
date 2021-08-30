712035b75208a0f757cf12df5d21fcb49196f5e9
lucasrafaeldesouza <56982485+lucasrafaeldesouza@users.noreply.github.com>
2021-08-30 12:07:17 -0300

    Update and rename atividade03_volume.txt to atividade03_volume.java

--- atividade03_volume.java
+++ atividade03_volume.java
@@ -1,12 +1,10 @@
-public class volume {
-      public static void main(String args[]) {
-        
+public class volume{
+      public static void main(String args[]){
         
         double  largura = 200.5;
         double altura = 75.5;
         double profundidade = 50.5;
-       
-        
+      
         double volume =  (largura * altura * profundidade);
         
         System.out.println(volume);
