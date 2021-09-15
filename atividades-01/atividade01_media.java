4f3f7599ded14e64da96eabedd9c3d9a581dc6ef
lucasrafaeldesouza <56982485+lucasrafaeldesouza@users.noreply.github.com>
2021-09-14 21:16:11 -0300

    Update atividade01_media.java

--- atividades-01/atividade01_media.java
+++ atividades-01/atividade01_media.java
@@ -1,7 +1,6 @@
 public class media {
       public static void main(String args[]) {
         
-        
         int nota1 = 8;
         int nota2 = 7;
         int nota3 = 10;
@@ -10,5 +9,4 @@ public class media {
         int media = (nota1 + nota2 + nota3 + nota4)/4;
         
         System.out.println(media);
-    }
-}
+   }
