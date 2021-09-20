6536423d9eb8551a253bdaac4e0f7a7d2edaa328
lucasrafaeldesouza <56982485+lucasrafaeldesouza@users.noreply.github.com>
2021-09-20 19:09:35 -0300

    Add files via upload

---
+++ atividades-01/conversor.java
@@ -0,0 +1,15 @@
+public class ConverterStringParaInt {
+
+    public static void main(String[] args) {
+
+        String numero = "25";
+
+        int numeroConvertido = Integer.parseInt(numero);
+
+        int numeroVezes2 = numeroConvertido * 2;
+
+        System.out.println( numeroVezes2);
+
+    }
+
+}
