340ab24000b3fb1c211ac02b364cf32737d1148f
lucasrafaeldesouza <56982485+lucasrafaeldesouza@users.noreply.github.com>
2021-12-07 21:44:24 -0300

    Add files via upload

---
+++ JavaZoo/model/Golfinho.java
@@ -0,0 +1,55 @@
+package model;
+
+import java.util.Objects;
+
+public class Golfinho extends Animal{
+
+    private int treinamento;
+    protected Jaula jaula;
+
+    public Golfinho (int id, String nome, int treinamento, int idJaula, String descricao) {
+        super(id, nome);
+        this.treinamento = treinamento;
+        this.jaula = new Jaula(idJaula, descricao);
+    }
+
+    public int getTreinamento() {
+        return this.treinamento;
+    }
+
+    public void setTreinamento(int treinamento) {
+        this.treinamento = treinamento;
+    }  
+
+    public Jaula getJaula() {
+        return this.jaula;
+    }
+
+    public void setJaula(Jaula jaula) {
+        this.jaula = jaula;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this)
+            return true;
+        if (!(o instanceof Golfinho)) {
+            return false;
+        }
+        Golfinho golfinho = (Golfinho) o;
+        return this.getId() == golfinho.getId();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.getId(), this.getNome());
+    }
+
+    @Override
+    public String toString() {
+        return "\n" +
+            "ID  = '" + getId() + "'" +
+            "\nTreinamento = '" + getTreinamento() + "'";
+            
+    }
+}
---
+++ JavaZoo/model/Treinamento.java
@@ -0,0 +1,76 @@
+package model;
+
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.Objects;
+
+public class Treinamento {
+
+    int id;
+    Date data;
+    String detalhes;
+    ArrayList<Golfinho> golfinho = new ArrayList<>();
+
+    public Treinamento (int id, Date data, String detalhes) {
+
+        this.id = id;
+        this.data = data;
+        this.detalhes = detalhes;
+    }
+    
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Date getData() {
+        return this.data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+
+    public String getDetalhes() {
+        return this.detalhes;
+    }
+
+    public void setDetalhes(String detalhes) {
+        this.detalhes = detalhes;
+    }
+
+    public void setGolfinho(Golfinho golfinho) {
+        this.golfinho.add(golfinho);    
+    }
+
+    public ArrayList<Golfinho> getGolfinho() {
+        return this.golfinho;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this)
+            return true;
+        if (!(o instanceof Treinamento)) {
+            return false;
+        }
+        Treinamento treinamento = (Treinamento) o;
+        return treinamento.getId() == this.getId();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.getId(), this.getData());
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+            " Leao ='" + getId() + "'" +
+            " id ='" + getId() + "'" +
+            "}";
+    }
+}
