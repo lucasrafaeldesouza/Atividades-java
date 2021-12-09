19c758c0fa0cbda5e0a9d2a980a4673acb969e74
lucasrafaeldesouza <56982485+lucasrafaeldesouza@users.noreply.github.com>
2021-12-09 19:02:32 -0300

    Update LeaoBanco.java

--- JavaZoo/LeaoBanco.java
+++ JavaZoo/LeaoBanco.java
@@ -1,16 +1,16 @@
+package model;
+
 import java.sql.Connection;
-import java.sql.Date;
+/*import java.sql.Date;*/
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
+/*import java.sql.Statement;*/
 import java.util.Scanner;
 
-import model.Leao;
-
 public class LeaoBanco {
-    /* private static int executeUpdate; */
+    /*private static int executeUpdate;*/
 
     public static void main(String[] args) {
         try {
@@ -31,7 +31,7 @@ public class LeaoBanco {
                 System.out.println("\t=================");
                 System.out.println("\t    JAVA - Zoológico ");
                 System.out.println("\t=================");
-                System.out.println(" [1] - INSERT - LEAO (Statement)");
+                System.out.println(" [1] - INSERT - LEAO (PreparedStatement)");
                 System.out.println(" [2] - UPDATE - LEAO (PreparedStatement)");
                 System.out.println(" [3] - DELETE - LEAO (PreparedStatement)");
                 System.out.println(" [4] - SELECT - LEAO (PreparedStatement)");
@@ -40,6 +40,7 @@ public class LeaoBanco {
                 escolha = scanner.nextInt();
 
                 /* TODA INSERÇÃO, UPDATE, DELETE E SELECT DO LEAO */
+                
                 switch (escolha) {
                     case 1:
 
@@ -72,20 +73,31 @@ public class LeaoBanco {
                         } catch (Exception e) {
                             System.out.println(e.getMessage());
                         }
-
+                        
+                        
                         PreparedStatement statement = con.prepareStatement(
-                                "INSERT INTO leao (nome, alimentacao, visitantes, jaula_id) VALUES (?,?,?,?)",
-                                PreparedStatement.RETURN_GENERATED_KEYS);
+                            "INSERT INTO leao (nome, alimentacao, visitantes, jaula_id) VALUES (?,?,?,?)",
+                            PreparedStatement.RETURN_GENERATED_KEYS
+                        );
 
                         PreparedStatement pStatement = con.prepareStatement(
-                                "INSERT INTO jaula (descricao) VALUES (?);",
-                                PreparedStatement.RETURN_GENERATED_KEYS);
+                            "INSERT INTO jaula (descricao) VALUES (?);",
+                            PreparedStatement.RETURN_GENERATED_KEYS
+                        );
+                        
+                        /*
+                        Statement statement = con.createStatement();
+                        statement.executeUpdate("INSERT INTO leao (nome, alimentacao, visitantes, descricao) VALUES ('"+nome+"',"+alimentacao+","+visitantes+",'"+descricao+"'");
+                        System.out.println("CADASTRADO REALIZADO COM SUCESSSO!");
+                        break;
+                        */
+
                         pStatement.setString(1, descricao);
                         if (pStatement.executeUpdate() > 0) {
                             ResultSet rs = pStatement.getGeneratedKeys();
                             rs.next();
                             int idJaula = rs.getInt(1);
-
+                            
                             statement.setString(1, nome);
                             statement.setInt(2, alimentacao);
                             statement.setInt(3, visitantes);
@@ -96,20 +108,19 @@ public class LeaoBanco {
                                 rsLeao.next();
                                 int idLeao = rs.getInt(1);
 
-                                ResultSet sqlLeao = con.createStatement()
-                                        .executeQuery("SELECT * FROM leao WHERE id = " + idLeao);
+                                ResultSet sqlLeao = con.createStatement().executeQuery("SELECT * FROM leao WHERE id = " + idLeao);
                                 sqlLeao.next();
                                 System.out.println(
-                                        new Leao(
-                                                sqlLeao.getInt("id"),
-                                                sqlLeao.getString("nome"),
-                                                sqlLeao.getInt("alimentacao"),
-                                                sqlLeao.getInt("visitantes"),
-                                                sqlLeao.getString("descricao")));
+                                    new Leao(
+                                        sqlLeao.getInt("id"),
+                                        sqlLeao.getString("nome"),
+                                        sqlLeao.getInt("alimentacao"),
+                                        sqlLeao.getInt("visitantes"), idJaula, descricao));
                             }
-                        }
+                        }                        
                         System.out.println("CADASTRADO REALIZADO COM SUCESSSO!");
                         break;
+                        
 
                     case 2:
 
@@ -143,11 +154,12 @@ public class LeaoBanco {
                         }
 
                         PreparedStatement preparedStatement2 = con.prepareStatement(
-                                "UPDATE leao SET nome = ?, alimentacao = ?, visitantes = ? WHERE id = ?",
-                                PreparedStatement.RETURN_GENERATED_KEYS);
+                        "UPDATE leao SET nome = ?, alimentacao = ?, visitantes = ? WHERE id = ?",
+                        PreparedStatement.RETURN_GENERATED_KEYS);
                         preparedStatement2.setString(1, nome);
                         preparedStatement2.setInt(2, alimentacao);
                         preparedStatement2.setInt(3, visitantes);
+                        preparedStatement2.setInt(4,id);
 
                         if (preparedStatement2.executeUpdate() <= 0) {
                             System.out.println("Falha ao alterar o leao");
@@ -184,9 +196,8 @@ public class LeaoBanco {
                             System.out.println(e.getMessage());
                         }
 
-                        PreparedStatement preparedStatement4 = con.prepareStatement(
-                                "SELECT * FROM leao WHERE id = ? ",
-                                PreparedStatement.RETURN_GENERATED_KEYS);
+                        PreparedStatement preparedStatement4 = con.prepareStatement("SELECT * FROM leao WHERE id = ? ",
+                        PreparedStatement.RETURN_GENERATED_KEYS);
                         preparedStatement4.setInt(1, id);
                         ResultSet result = preparedStatement4.executeQuery();
                         while (result.next()) {
@@ -195,7 +206,8 @@ public class LeaoBanco {
                                     result.getString("nome"),
                                     result.getInt("alimentacao"),
                                     result.getInt("visitantes"),
-                                    result.getString("descricao"));
+                                    result.getInt("jaula_id"), descricao);
+                                    
                             System.out.println(leao);
                         }
                         break;
